06cb1affd4c277885eb20d7935c0af778bc92d24
mdemartino26 <m.demartino99@gmail.com>
2022-06-07 17:42:32 -0300

    cambios

--- assets/javascript.js
+++ assets/javascript.js
@@ -5,6 +5,8 @@ var array=["-10 Millones", "-20 Millones", "-5 Millones", "-15 Millones", "- 25 
 function Atacar(){
 var oculto = document.getElementById("atacar");
 oculto.setAttribute("class", "noShow");
+var mostrar = document.getElementById("atacado");
+mostrar.setAttribute("class", "");
 var random = array[Math.floor(Math.random() * array.length)];
 console.log(random);
 
