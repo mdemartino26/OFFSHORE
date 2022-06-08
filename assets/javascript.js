ceef611981bcfaff425ad356d681dfd474af29c4
mdemartino26 <m.demartino99@gmail.com>
2022-06-07 21:28:30 -0300

    loader

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,18 @@
 var array=["-10 Millones", "-20 Millones", "-5 Millones", "-15 Millones", "- 25 Millones"];
 
-
+function Loader(){
+    setTimeout(Atacar, 3000);
+    var oculto = document.getElementById("atacar");
+    oculto.setAttribute("class", "noShow");
+    var loader = document.getElementById("loader");
+    loader.setAttribute("class", "loader");
+}
 
 function Atacar(){
 var oculto = document.getElementById("atacar");
 oculto.setAttribute("class", "noShow");
+var loader = document.getElementById("loader");
+    loader.setAttribute("class", "noShow");
 var mostrar = document.getElementById("atacado");
 mostrar.setAttribute("class", "");
 var random = array[Math.floor(Math.random() * array.length)];
